26f6ddcd7c741add99f775801501a81a93dfd3d3
gigiricca <gissellericca@gmail.com>
2024-09-07 15:07:35 -0300

    Cambio en bbdd

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "root",
-    password: "1234",
-    database: "cateringlink",
-    host: "127.0.0.1",
+    username: process.env.DB_USER || "admin", // Cambia a usar variable de entorno
+    password: process.env.DB_PASSWORD || "CateringL1nk#", // Cambia a usar variable de entorno
+    database: process.env.DB_NAME || "cateringlink", // Cambia a usar variable de entorno
+    host: process.env.DB_HOST || "database-1.c5e2kmgo41bv.us-east-2.rds.amazonaws.com", // Cambia a usar variable de entorno
     dialect: "mysql",
   },
 };
