51970f1156ef34910cad0bba9025df0be71974bc
gigiricca <gissellericca@gmail.com>
2024-09-08 12:25:40 -0300

    Se modifica server

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const cors = require('cors');
 
 // Configurar CORS de forma específica
 app.use(cors({
-    origin: 'https://www.cateringlink.store',  // Cambia esto al dominio de tu frontend en Vercel
+    origin: ['https://www.cateringlink.store', 'https://cateringlink.store'], // Cambia esto al dominio de tu frontend en Vercel
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos HTTP permitidos
     allowedHeaders: ['Content-Type', 'Authorization'],  // Encabezados permitidos
     credentials: true  // Permitir envío de cookies/credenciales
 }));
+
+app.options('*', cors());
 
 app.use("/api", routes);
 
