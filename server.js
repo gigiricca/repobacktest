2343e10d854fb115c9881665081eff37a135e385
gigiricca <gissellericca@gmail.com>
2024-09-08 14:13:46 -0300

    Se modifica SERVER

--- server.js
+++ server.js
@@ -8,22 +8,20 @@ app.use(express.json());
 
 // Configurar CORS de forma específica
 app.use(cors({
-    origin: ['https://www.cateringlink.store', 'https://cateringlink.store'], // Cambia esto al dominio de tu frontend en Vercel
+    origin: ['https://www.cateringlink.store', 'https://cateringlink.store'], // Dominios permitidos
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Encabezados permitidos
+    allowedHeaders: '*',  // Permitir todos los encabezados
     credentials: true  // Permitir envío de cookies/credenciales
 }));
-
-app.options('*', cors());
 
 app.use("/api", routes);
 
 const PORT = process.env.PORT || 8080;
-const HOST = '127.0.0.1';  // Cambia esto de '::' o '0.0.0.0' a '127.0.0.1'
+const HOST = '0.0.0.0';  // Permitir acceso desde cualquier red
 
 // Sincronizar modelos y luego iniciar el servidor
 sequelize.
-  sync({ alter: true })
+  sync()  // No usar { alter: true } en producción
   .then(() => {
     app.listen(PORT, HOST, () => {
       console.log(`Servidor escuchando en el puerto ${PORT}`);
@@ -33,7 +31,7 @@ sequelize.
     console.error("Error al sincronizar la base de datos:", err);
   });
 
-  // Definir una ruta para manejar solicitudes a "/"
+// Definir una ruta para manejar solicitudes a "/"
 app.get('/', (req, res) => {
   res.send('Bienvenido a la API'); 
 });
